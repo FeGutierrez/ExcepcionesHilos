1c88cc27edfaac9f3c42505ce6c68ca477def851
Estudiante <Estudiante@10.203.1.158>
2018-04-12 08:53:02 -0500

    Terminar

---
+++ src/co/unal/edu/poo/fgiraldo/fgutierrez/ExcepcionPersonalizada.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.unal.edu.poo.fgiraldo.fgutierrez;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class ExcepcionPersonalizada extends Exception{
+        
+    public ExcepcionPersonalizada(String mensaje){
+            super(mensaje);
+    }
+     
+}
